a18fb4f3cd99360530ea23ade78da0c505239a72
Pazulay <azulay3@yahoo.com>
2021-01-11 20:20:23 +0100

    Added files

---
+++ Block Run/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+
+        if (collisionInfo.collider.tag == "Obstacles") 
+        {  
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ Block Run/Assets/Scripts/Points.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+
+    void Update()
+    {
+        
+
+        scoreText.text = player.position.z.ToString("0");
+        
+    }
+}
